9485e3da14e03068a9a1be01d7a2696da70b0a88
Cliff Curry <ccurry@google.com>
2014-07-10 11:01:41 -0700

    Initial Commit

---
+++ hamburger/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  hamburger
+//
+//  Created by Cliff Curry on 7/10/14.
+//  Copyright (c) 2014 Cliff Curry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
